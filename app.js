205e174db58db5e6065a64ad278dc19f83d825f9
Kiki Saepul Ramdani <kikisaepulramdani38@gmail.com>
2020-10-23 09:59:57 +0700

    Install CORS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const cors = require("cors");
 // import mongoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://kikisaepulrmdni:mernstack@cluster0.mbt0c.mongodb.net/db-staycation?retryWrites=true&w=majority", {
@@ -36,6 +37,7 @@ app.use(
   })
 );
 app.use(flash());
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
